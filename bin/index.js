aa72b07da009ca80099986c7486d34ec851c5918
zhouchanglong_cd <zhouchanglong_cd@keruyun.com>
2018-10-08 13:51:25 +0800

    feat(i18n-tool):相关默认路径

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,12 @@
 #!/usr/bin/env node 
 const program = require('commander');
 const inquirer = require('inquirer');
+const path = require('path');
 const info = require('../package.json');
-const { batchWrite } = require('../lib/write');
-const { exportExcel } = require('../lib/excel');
+const { batchWrite, exportExcel } = require('../lib');
 
 program.allowUnknownOption();
-program.version(info.version);
-
-program.version('0.0.1', '-v, --version')
+program.version(info.version, '-v, --version')
     .command('write')
     .action(() => {
         inquirer.prompt([
@@ -22,7 +20,11 @@ program.version('0.0.1', '-v, --version')
             name: 'distdir',
             message: '请输入项目多语言目录路径:'
         }]).then(({ distdir, src_excel_path }) => {
-            batchWrite(distdir, src_excel_path);
+            if (!src_excel_path) {
+                throw new Error('Invalid path of source excel');
+            }
+            const dist_dir = distdir || path.join(process.cwd(), 'public/lang');
+            batchWrite(dist_dir, src_excel_path);
         });
     });
 
@@ -44,8 +46,10 @@ program.command('export')
             name: 'excel_name',
             message: '请输入excel文件名:'
         }
-      ]).then(({ src_dir, excel_name, lang_dir }) => {
-          exportExcel(src_dir, excel_name, lang_dir);
+      ]).then(({ src_dir = process.cwd(), excel_name, lang_dir }) => {
+          const filename = excel_name || 'messages';
+          const langdir = lang_dir || path.join(process.cwd(), 'public/lang');
+          exportExcel(src_dir, filename, langdir);
       });
 });
 program.parse(process.argv);
